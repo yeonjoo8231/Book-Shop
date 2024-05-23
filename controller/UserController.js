098978e064d708b6855993799c9ee29f3611ab2b
yeonjoo8231 <77268860+yeonjoo8231@users.noreply.github.com>
2024-05-23 19:32:00 +0900

    240523-2

--- controller/UserController.js
+++ controller/UserController.js
@@ -13,6 +13,7 @@ const join = (req, res) => {
   const hashPassword = cryto
     .pbkdf2Sync(password, salt, 10000, 10, "sha512")
     .toString("base64");
+
   let values = [email, hashPassword, salt];
 
   conn.query(sql, values, (err, results) => {
@@ -26,6 +27,87 @@ const join = (req, res) => {
   });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "SELECT * FROM users WHERE email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const loginUser = results[0];
+
+    const hashPassword = cryto
+      .pbkdf2Sync(password, loginUser.salt, 10000, 10, "sha512")
+      .toString("base64");
+
+    if (loginUser && loginUser.password == hashPassword) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "me",
+        }
+      );
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      console.log(token);
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+const passwordResetRequest = (req, res) => {
+  const { email } = req.body;
+  let sql = "SELECT * FROM users WHERE email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const user = results[0];
+    if (user) {
+      return res.status(StatusCodes.OK).json({
+        email,
+      });
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
+};
+const passwordReset = (req, res) => {
+  const { email, password } = req.body;
+
+  let sql = "UPDATE users SET password = ?, salt = ? WHERE email = ?";
+
+  const salt = cryto.randomBytes(10).toString("base64");
+  const hashPassword = cryto
+    .pbkdf2Sync(password, salt, 10000, 10, "sha512")
+    .toString("base64");
+
+  const values = [hashPassword, salt, email];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.affectedRows == 0) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    } else {
+      return res.status(StatusCodes.OK).json(results);
+    }
+  });
+};
+
 module.exports = {
   join,
+  login,
+  passwordResetRequest,
+  passwordReset,
 };
